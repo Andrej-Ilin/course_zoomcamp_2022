97b67eb53230db7141555c0a751a29474c665fbf
Andrei <andrej_ilin7@mail.ru>
2022-12-15 22:14:58 +0300

    add_pred

--- capstone1/predict.py
+++ capstone1/predict.py
@@ -1,5 +1,43 @@
+import tensorflow.lite as tflite
+from keras_image_helper import create_preprocessor
+import pickle
 import numpy as np
 
-import tensorflow as tf
-from tensorflow import keras
+preprocessor = create_preprocessor('xception', target_size=(224, 224))
 
+interpreter = tflite.Interpreter(model_path='model_0.936.tflite')
+interpreter.allocate_tensors()
+
+input_index = interpreter.get_input_details()[0]['index']
+output_index = interpreter.get_output_details()[0]['index']
+
+with open("classes", "rb") as fp:   # Unpickling
+    classes = pickle.load(fp)
+
+
+url = 'https://glorypets.ru/wp-content/uploads/2020/07/1-tsarstvennost.jpg'
+
+
+
+def predict(url):
+    X = preprocessor.from_url(url)
+
+    interpreter.set_tensor(input_index, X)
+    interpreter.invoke()
+    preds = interpreter.get_tensor(output_index)
+
+    ind = np.argpartition(preds[0], -2)[-2:]
+    print(ind)
+    float_predictions = preds[0].tolist()
+
+    return dict(zip(classes, float_predictions))
+
+
+def lambda_handler(event, context):
+    url = event['url']
+    result = predict(url)
+    return result
+
+
+
+#%%
